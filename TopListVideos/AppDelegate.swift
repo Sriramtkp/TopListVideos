d3feaaa5e0a59b02106a48b04f0e6daee00c757f
Sriram Rajendran <sriramrajendrantkp@yahoo.in>
2016-10-10 01:00:23 +0530

    Part-8 Reachability did but need some corrections

--- TopListVideos/AppDelegate.swift
+++ TopListVideos/AppDelegate.swift
@@ -8,23 +8,66 @@
 
 import UIKit
 
+//globalVars
+
+var reachGlObj = Reachability?()
+
+var reachStatusGlObj = ""
+
+
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
 
 
+  var internetCheckGlObj = Reachability?()
+  
+  
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
     // Override point for customization after application launch.
     
     
 //    NSURLCache.setSharedURLCache(NSURLCache(memoryCapacity: 0, diskCapacity: 0, diskPath: nil))
     
+    NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(AppDelegate.reachChanged(_:)), name: kReachabilityChangedNotification, object: nil)
     
+    internetCheckGlObj = Reachability.reachabilityForInternetConnection()
+    internetCheckGlObj?.startNotifier()
     
     return true
   }
 
+  
+  //customFuncs
+  
+  func reachChanged(notify: NSNotification){
+    
+    reachGlObj = notify.object as? Reachability
+    statusChange(reachGlObj!)
+    
+  }
+  
+  func statusChange(currentReachStaus: Reachability)  {
+    
+    let netStatus : NetworkStatus = currentReachStaus.currentReachabilityStatus()
+    
+    switch netStatus.rawValue {
+    case NotReachable.rawValue : reachStatusGlObj = NOACCESS
+    case ReachableViaWiFi.rawValue : reachStatusGlObj = WIFI
+    case ReachableViaWWAN.rawValue : reachStatusGlObj = WANN
+    default: return
+    }
+    
+    NSNotificationCenter.defaultCenter().postNotificationName("ReachStatusChanged", object: nil)
+    
+    
+  }
+  
+  
+  
+  
   func applicationWillResignActive(application: UIApplication) {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
@@ -45,6 +88,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func applicationWillTerminate(application: UIApplication) {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    
+    NSNotificationCenter.defaultCenter().removeObserver(self, name: kReachabilityChangedNotification, object: nil)
+    
+    
   }
 
 
